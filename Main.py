92ac1ea844db4374e930b54938c7b19df8f486ff
landerosn <landerosn@berea.edu>
2022-04-29 01:57:52 -0400

    scatterplot

--- Main.py
+++ Main.py
@@ -9,8 +9,21 @@ from datetime import datetime
 import timestamp as timestamp
 #%matplotlib inline
 
-data = pd.read_csv('C:\Users\landerosn\Documents\Berea College Schoolwork\Year 2022\CSC 486')
+data = pd.read_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv')
 print(data.shape)
 print(data.head(10))
-data['event_date'] = data['event_time'].apply(lambda s: convert_time_to_date(s))
-visitor_by_date = data[['event_date','user_id']].drop_duplicates().groupby(['event_date'])['user_id'].agg(['count']).sort_values(by=['event_date'], ascending=True)
+
+cor2=data.corr()
+print(cor2)
+
+# df2=pd.DataFrame(cor2)
+# df2.to_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv')
+
+x = data['user_id']
+y = data['event_type']
+plt.xlabel("user_id")
+plt.ylabel("event_type")
+plt.scatter(x,y)
+plt.show()
+# data['event_date'] = data['event_time'].apply(lambda s: convert_time_to_date(s))
+# visitor_by_date = data[['event_date','user_id']].drop_duplicates().groupby(['event_date'])['user_id'].agg(['count']).sort_values(by=['event_date'], ascending=True)
