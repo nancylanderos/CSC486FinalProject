a9157421a3fffc4f259132c22e080dc193c06820
landerosn <landerosn@berea.edu>
2022-05-01 19:00:53 -0400

    plots

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import squarify
 import matplotlib.dates as dates
 from datetime import datetime
+from pylab import *
+import networkx as nx
 
 import timestamp as timestamp
 #%matplotlib inline
@@ -14,22 +16,22 @@ np.set_printoptions(linewidth=desired_width)
 pd.set_option('display.max_columns',10)
 
 data = pd.read_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv')
-print(data.shape)
-print(data.head(10))
-
-cor2=data.corr()
-print(cor2)
+# print(data.shape)
+# print(data.head(10))
+#
+# cor2=data.corr()
+# print(cor2)
 
 # df2=pd.DataFrame(cor2)
 # df2.to_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv')
 
 #Users and status on viewing for items
-# x = data['user_id']
-# y = data['event_type']
-# plt.xlabel("user_id")
-# plt.ylabel("event_type")
-# plt.scatter(x,y)
-# plt.show()
+x = data['user_session']
+y = data['event_type']
+plt.xlabel("user_session")
+plt.ylabel("event_type")
+plt.scatter(x,y)
+plt.show()
 
 #category customers find/view
 # topcateg = 30
@@ -67,20 +69,41 @@ print(cor2)
 # plt.legend()
 # plt.show()
 
-#what items are purchased?
+#what items are purchased? /graph a bar chart showing popular items under purchased
 # purchase = data.loc[data['event_type'] == "purchase"]
 # purchase1 = purchase.dropna(axis = 'rows')
 # print (purchase1)
-#what items are viewed?
+
+#example
+
+#what items are viewed? / graph a bar chart showing popular items under viewed
+
+# viewed = data.loc[data['event_type'] == 'view']
+# viewed1 = viewed.dropna(axis = 'rows')
+# print (viewed1)
 
 #is there a correlation between the ones viewed and purchased?
 
-#brands that customers purchase
+#brands that customers purchase shown in console
 # topsellers = purchase.groupby('brand')['brand'].agg(['count']).sort_values('count', ascending=False)
 # topsellers.head(20)
 # print(topsellers)
-
+# #brands that customers purchase attempt to create graph
+# labels = ['purchase']
+# size = data['event_type'].value_counts()
+# colors = ['green', 'blue', 'teal', 'pink']
+# explode = [0, 0.1, 0.1]
+# plt.rcParams['figure.figsize'] = (8,8)
+# plt.pie(size, colors=colors, explode=explode, labels=labels)
+# plt.title('Purchased items', fontsize=14)
+# plt.axis('off')
+# plt.legend()
+# plt.show()
 #example of a user behavior
-user_session = 	513351129
-data.loc[data['user_id'] == user_session]
-print (data.loc[data['user_id'] == user_session])
+# user_session = 	513351129
+# data.loc[data['user_id'] == user_session]
+# print (data.loc[data['user_id'] == user_session])
+
+# g = nx.read_adjlist('C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv', delimiter=',')
+# nx.draw(g, with_labels=True)
+# show()
