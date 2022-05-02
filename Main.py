52626338103d4d4415f95c9ac611a1adcccd1634
landerosn <landerosn@berea.edu>
2022-05-02 00:59:12 -0400

    dataframe

--- Main.py
+++ Main.py
@@ -26,12 +26,20 @@ data = pd.read_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 
 # df2.to_csv(r'C:/Users/landerosn/Documents/Berea College Schoolwork/Year 2022/CSC 486/2019-Nov.csv')
 
 #Users and status on viewing for items
-x = data['user_session']
-y = data['event_type']
-plt.xlabel("user_session")
-plt.ylabel("event_type")
-plt.scatter(x,y)
-plt.show()
+# x = data['user_id']
+# y = data['event_type']
+# plt.xlabel("user_id")
+# plt.ylabel("event_type")
+# plt.scatter(x,y)
+# plt.show()
+
+#price and event type scatterplot
+# x = data['price']
+# y = data['event_type']
+# plt.xlabel("price")
+# plt.ylabel("event_type")
+# plt.scatter(x,y)
+# plt.show()
 
 #category customers find/view
 # topcateg = 30
@@ -40,7 +48,13 @@ plt.show()
 # squarify.plot(sizes=topcateg1, label=topcateg1.index.array, color=["red","blue","green","grey","cyan"], alpha=.7)
 # plt.axis('off')
 # plt.show()
-
+#brands customers navigate through
+topcateg = 30
+topcateg1 = data['brand'].value_counts()[:topcateg].sort_values(ascending=False)
+df = pd.DataFrame({'count':topcateg1, "topcateg1":topcateg1})
+squarify.plot(sizes=topcateg1, label=topcateg1.index.array, color=["red","blue","green","grey","cyan"], alpha=.7)
+plt.axis('off')
+plt.show()
 
 #showing traffic/work on it
 # def convert_time_to_date(utc_timestamp):
@@ -72,16 +86,18 @@ plt.show()
 #what items are purchased? /graph a bar chart showing popular items under purchased
 # purchase = data.loc[data['event_type'] == "purchase"]
 # purchase1 = purchase.dropna(axis = 'rows')
-# print (purchase1)
+# print (purchase)
 
-#example
 
 #what items are viewed? / graph a bar chart showing popular items under viewed
 
 # viewed = data.loc[data['event_type'] == 'view']
 # viewed1 = viewed.dropna(axis = 'rows')
 # print (viewed1)
-
+#what items are placed in a cart?
+# cart = data.loc[data['event_type'] == "cart"]
+# cart1 = cart.dropna(axis = 'rows')
+# print (cart1)
 #is there a correlation between the ones viewed and purchased?
 
 #brands that customers purchase shown in console
